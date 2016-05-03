e39a4c661b4d824659cd26f0442032cf736f79e9
Nick Weiland <nickweiland@gmail.com>
2016-05-03 10:13:11 -0700

    testttt

--- app/purgatory_challenges/divinization.rb
+++ app/purgatory_challenges/divinization.rb
@@ -4,13 +4,16 @@ module Divinization
   extend ActiveSupport::Concern
 
   def unique_merchant_ids
+    "test"
   end
 
   class_methods do
     def pluck_first_names
+      :test
     end
 
     def pluck_last_names_in_reverse_alphabetical_order
+      :test
     end
   end
 end
